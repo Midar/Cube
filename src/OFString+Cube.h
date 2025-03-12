4aed51a95d21a603bc2a0ce9b01232549d5cea3a
Jonathan Schleifer <js@nil.im>
2025-03-12 22:35:55 +0000

    Add files forgotten in last commit
    FossilOrigin-Name: e208417f95180ec74e3c5376d6f502f3cdb0a356c6541458e8864a3fefa5cd80

---
+++ src/OFString+Cube.h
@@ -0,0 +1,8 @@
+#import <ObjFW/ObjFW.h>
+
+@interface
+OFString (Cube)
+@property (readonly, nonatomic) int cube_intValue;
+
+- (int)cube_intValueWithBase:(unsigned char)base;
+@end
---
+++ src/OFString+Cube.mm
@@ -0,0 +1,32 @@
+#import "OFString+Cube.h"
+
+#include "cube.h"
+
+@implementation
+OFString (Cube)
+- (int)cube_intValue
+{
+	@try {
+		return self.intValue;
+	} @catch (OFInvalidFormatException *e) {
+		conoutf(@"invalid value: %@", self);
+		return 0;
+	} @catch (OFOutOfRangeException *e) {
+		conoutf(@"invalid value: %@", self);
+		return 0;
+	}
+}
+
+- (int)cube_intValueWithBase:(unsigned char)base
+{
+	@try {
+		return [self intValueWithBase:base];
+	} @catch (OFInvalidFormatException *e) {
+		conoutf(@"invalid value: %@", self);
+		return 0;
+	} @catch (OFOutOfRangeException *e) {
+		conoutf(@"invalid value: %@", self);
+		return 0;
+	}
+}
+@end
